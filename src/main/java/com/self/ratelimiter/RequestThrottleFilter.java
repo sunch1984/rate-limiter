e8317f32d277171114bac8eac74f417998a637af
sunch1984 <sunch1984@126.com>
2021-01-29 10:44:21 +0800

    init project

---
+++ src/main/java/com/self/ratelimiter/RequestThrottleFilter.java
@@ -0,0 +1,32 @@
+package com.self.ratelimiter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import com.google.common.util.concurrent.RateLimiter;
+
+@Component
+public class RequestThrottleFilter implements Filter {
+
+	final RateLimiter rateLimiter = RateLimiter.create(2);
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		if (!rateLimiter.tryAcquire(1)) {
+			chain.doFilter(request, response);
+		} else {
+			httpServletResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+			httpServletResponse.getWriter().write("Too many requests");
+		}
+
+	}
+}
